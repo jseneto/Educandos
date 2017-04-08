bccb860d6308b223afc9977ab4563329412dad33
José Neto <josenetotj@hotmail.com>
2017-04-08 00:33:33 -0300

    criacao de screen de gerenciamento de matriculas

---
+++ app/src/main/java/com/example/jose/educandos/screens/MinhasMatriculas.java
@@ -0,0 +1,13 @@
+package com.example.jose.educandos.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinhasMatriculas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minhas_matriculas);
+    }
+}
