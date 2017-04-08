0cb4d9030ad192cd0c42e6cbcdef6e26f51fbff1
José Neto <josenetotj@hotmail.com>
2017-04-08 00:32:34 -0300

    criacao de screen de gerenciamento de todas as turmas

---
+++ app/src/main/java/com/example/jose/educandos/screens/TodasAsTurmas.java
@@ -0,0 +1,13 @@
+package com.example.jose.educandos.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodasAsTurmas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todas_as_turmas);
+    }
+}
